af63f745ec5ef0c3574a8f78f69496053c9f8358
Dominik Stadler <dominik.stadler@dynatrace.com>
2023-09-26 08:15:14 +0200

    Reduce source-code some more

--- src/test/java/org/dstadler/commoncrawl/datalayer/DataAccessFactoryTest.java
+++ src/test/java/org/dstadler/commoncrawl/datalayer/DataAccessFactoryTest.java
@@ -1,10 +1,25 @@
 package org.dstadler.commoncrawl.datalayer;
 
+import static org.junit.Assert.assertTrue;
+
 import javax.persistence.EntityManager;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class DataAccessFactoryTest extends DatabaseBase {
+public class DataAccessFactoryTest {
+	public static DatabaseStarter starter = new DatabaseStarter();
+
+	@BeforeClass
+	public static void startDB() {
+		starter.start();
+	}
+
+	@AfterClass
+	public static void stopDB() {
+		assertTrue(starter.stop());
+	}
 
 	@Test
 	public void testGetInstance() {
